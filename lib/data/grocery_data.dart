d18cbd491f582ae02399ef7092c4ea760e145e5b
Mohammad Joynul Abedin <shokal.ash21016@gmail.com>
2023-05-02 14:46:54 +0600

    Persis Database implemented

--- lib/data/grocery_data.dart
+++ lib/data/grocery_data.dart
@@ -52,25 +52,3 @@ class GroceryData {
     }
   }
 }
-
-// class GroceryData {
-//   static List<Map<String, dynamic>> groceryProducts = [];
-//
-//   Future<void> fetchPhotos() async {
-//     final url = Uri.https('makeup-api.herokuapp.com', '/api/v1/products.json',
-//         {'brand': 'marienatie'});
-//     final response = await http.get(url);
-//     final List<dynamic> data = json.decode(response.body);
-//     if (data is List && data.isNotEmpty) {
-//       for (final product in data) {
-//         if (product is Map<String, dynamic>) {
-//           groceryProducts.add(product);
-//         } else {
-//           throw Exception('Invalid JSON data format');
-//         }
-//       }
-//     } else {
-//       throw Exception('Failed to load photos');
-//     }
-//   }
-// }
