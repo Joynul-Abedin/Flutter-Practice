0821de1b9e2cd2e1f6983c807500dbebfda2cc51
Mohammad Joynul Abedin <shokal.ash21016@gmail.com>
2023-05-02 14:29:59 +0600

    Persis Database implemented

--- lib/data/grocery_data.dart
+++ lib/data/grocery_data.dart
@@ -13,19 +13,12 @@ class GroceryData {
 
   void databaseInit() async {
     database = openDatabase(
-      // Set the path to the database. Note: Using the `join` function from the
-      // `path` package is best practice to ensure the path is correctly
-      // constructed for each platform.
       join(await getDatabasesPath(), 'database.db'),
-      // When the database is first created, create a table to store dogs.
       onCreate: (db, version) {
-        // Run the CREATE TABLE statement on the database.
         return db.execute(
           'CREATE TABLE photos(albumId INTEGER, id INTEGER PRIMARY KEY,  title TEXT, thumbnailUrl TEXT, url TEXT)',
         );
       },
-      // Set the version. This executes the onCreate function and provides a
-      // path to perform database upgrades and downgrades.
       version: 1,
     );
   }
